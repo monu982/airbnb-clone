d2ae1157977e25210df2e1e62dbf8719e6e59cb3
Manish Singh <manisharmy004@gmail.com>
2025-01-02 13:00:56 +0530

    update

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+// const helmet = require("helmet");
+// app.use(helmet());
+
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
